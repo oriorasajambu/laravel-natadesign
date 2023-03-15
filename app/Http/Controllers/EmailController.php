5af96739655a65b54d675909dcf9d606f93aea9b
Rio Wirawan <oriorasajambu@gmail.com>
2023-03-15 14:46:53 +0700

    debug: form

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -37,6 +37,7 @@ class EmailController extends Controller
 
     public function inquiryForm(InquiryFormRequest $request)
     {
+        dd($request);
         $mail = $request->all();
         try {
             Mail::to($this->target)->send(new InquiryPrice($mail));
