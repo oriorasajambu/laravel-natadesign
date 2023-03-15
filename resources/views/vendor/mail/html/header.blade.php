d574626fa960ded1156a72f2846c0d7eb97e9896
Rio Wirawan <oriorasajambu@gmail.com>
2023-03-15 14:41:21 +0700

    fix: icon email 2

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,11 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('assets/favicon.svg') }}" class="logo" alt="Laravel Logo">
+@if (trim($slot) === 'Laravel')
+<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+@else
+{{ $slot }}
+@endif
 </a>
 </td>
 </tr>
