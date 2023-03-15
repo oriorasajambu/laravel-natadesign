4639c352fcf4f956603efbe6553f03273df881af
Rio Wirawan <oriorasajambu@gmail.com>
2023-03-15 14:35:19 +0700

    fix: icon email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -5,7 +5,7 @@
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
-{{ $slot }}
+<img src="{{ asset('assets/favicon.svg') }}" class="logo" alt="Laravel Logo">
 @endif
 </a>
 </td>
