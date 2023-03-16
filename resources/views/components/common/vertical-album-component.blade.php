de859f4981339dffb9db9cdf5a2fd7ec7b99705c
Rio Wirawan <oriorasajambu@gmail.com>
2023-03-16 16:59:44 +0700

    fix: alt image portofolio

--- resources/views/components/common/vertical-album-component.blade.php
+++ resources/views/components/common/vertical-album-component.blade.php
@@ -3,7 +3,7 @@
         <a aria-label="{{ $album->title }}" href="/portofolio/{{ $album->slug }}">
             <div class="grid grid-cols-1">
                 <h2 class="font-dm-sans font-bold text-partial text-2xl">{{ $album->title }}</h2>
-                <p class="text-partial font-dm-sans text-xs leading-6 line-clamp-3 my-6">{{ $album->image }}</p>
+                <p class="text-partial font-dm-sans text-xs leading-6 line-clamp-3 my-6">{{ $album->alt }}</p>
                 <div class="w-full h-auto aspect-square relative">
                     <img src="https://admin.natadesign.id/{{ $album->image }}" alt="{{ $album->alt }}" />
                 </div>
