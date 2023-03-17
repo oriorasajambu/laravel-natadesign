00a9f434acb2fa5b949a84f77fd44484def0049b
Rio Wirawan <oriorasajambu@gmail.com>
2023-03-17 22:02:27 +0700

    fix: icon size

--- resources/views/components/bottom-bar-component.blade.php
+++ resources/views/components/bottom-bar-component.blade.php
@@ -13,7 +13,10 @@
                 <li>
                     <a href="https://www.instagram.com/nat.adesign" rel="external" aria-label="Instagram" target="_blank"
                         tabIndex={6} class="block py-2 pl-3 pr-4 text-white font-dm-serif">
-                        <i class="fa-brands fa-facebook-f fa-lg"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12.5" height="21" viewBox="0 0 320 512">
+                            <path
+                                d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" />
+                        </svg>
                     </a>
                 </li>
                 <li>
