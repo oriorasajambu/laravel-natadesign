b8e56517df3749b290ba8ccb040d743dfb49ae51
Rio Wirawan <oriorasajambu@gmail.com>
2023-03-17 15:07:48 +0700

    fix: manifest json route

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="manifest" href="{{ asset('assets/manifest.json') }}">
+    <link rel="manifest" href="{{ asset('manifest.json') }}">
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
     @yield('head')
