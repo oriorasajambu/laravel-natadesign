5da33ccf007557265211915bb5d2014916366b17
Rio Wirawan <oriorasajambu@gmail.com>
2023-03-17 08:32:10 +0700

    fix: check null on delete

--- resources/views/components/common/article-component.blade.php
+++ resources/views/components/common/article-component.blade.php
@@ -6,8 +6,8 @@
                     href="{{ $article->category->name == 'Ulasan Media' ? '/media' : '/articles' }}/{{ $article->slug }}">
                     <article class="flex flex-col gap-6">
                         <x-common.date-component date='{{ $article->published_at }}' />
-                        <img src="https://admin.natadesign.id/{{ $article->thumbnail->image }}" class="basis-[19%] w-full grow aspect-square"
-                            alt="{{ $article->thumbnail->alt }}" />
+                        <img src="https://admin.natadesign.id/{{ isset($article->thumbnail->image) ? $article->thumbnail->image : '' }}" class="basis-[19%] w-full grow aspect-square"
+                            alt="{{ isset($article->thumbnail->alt) ? $article->thumbnail->alt : '' }}" />
                         <h2 class="text-partial font-dm-sans font-bold text-xl truncate">{{ $article->title }}</h2>
                         <div class="text-secondary font-dm-sans text-base leading-6 line-clamp-3">
                             {!! $article->content !!}
@@ -23,8 +23,8 @@
                         <div class="text-secondary font-dm-sans text-base leading-6 line-clamp-3">
                             {!! $article->content !!}
                         </div>
-                        <img src="https://admin.natadesign.id/{{ $article->thumbnail->image }}" class="basis-[19%] w-full grow aspect-square"
-                            alt="{{ $article->thumbnail->alt }}" />
+                        <img src="https://admin.natadesign.id/{{ isset($article->thumbnail->image) ? $article->thumbnail->image : '' }}" class="basis-[19%] w-full grow aspect-square"
+                            alt="{{ isset($article->thumbnail->alt) ? $article->thumbnail->alt : '' }}" />
                     </article>
                 </a>
             @endif
