5905c8f7fb4c6b4725ec7bf233a67d52e8d4d156
Rio Wirawan <oriorasajambu@gmail.com>
2023-03-17 19:01:37 +0700

    fix: async

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,53 +16,10 @@
     @yield('content')
 
     <script src="https://kit.fontawesome.com/f7e5c3263d.js" crossorigin="anonymous" defer></script>
-    <script src="{{ asset('js/jquery/jquery.min.js') }}" async></script>
-    <script src="{{ asset('js/jquery/jquery-ui/jquery-ui.min.js') }}" async></script>
-    <script src="{{ asset('js/jquery/jquery-form.min.js') }}" async></script>
-
-    <script>
-        window.onload = function() {
-            $(document).ready(function() {
-                const formIdContact = "#formSendContact";
-                $(formIdContact).submit(function(event) {
-                    $("#btnSendContact").prop('disabled', true);
-                    $("#default-button-contact").addClass('hidden');
-                    $("#loading-button-contact").removeClass('hidden');
-                    $("#loading-button-contact").addClass('inline-flex');
-                    let formData = {
-                        name: $(formIdContact).find("input[name=name]").val(),
-                        email: $(formIdContact).find("input[name=email]").val(),
-                        question: $(formIdContact).find("textarea[name=question]").val(),
-                    }
-
-                    $.ajax({
-                        method: 'POST',
-                        url: "{{ route('send.contact') }}",
-                        headers: {
-                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                        },
-                        data: formData,
-                        success: function(data) {
-                            $("#btnSendContact").removeAttr('disabled');
-                            $("#loading-button-contact").removeClass('inline-flex');
-                            $("#loading-button-contact").addClass('hidden');
-                            $("#default-button-contact").removeClass('hidden');
-                            if (data.code == 200 && data.success) {
-                                //TODO Show Notif Success
-                                $(formIdContact).each(function() {
-                                    this.reset();
-                                });
-                            } else {
-                                //TODO Show Notif Failed
-                            }
-                        }
-                    });
-
-                    event.preventDefault();
-                });
-            })
-        }
-    </script>
+    <script src="{{ asset('js/jquery/jquery.min.js') }}" defer></script>
+    <script src="{{ asset('js/jquery/jquery-ui/jquery-ui.min.js') }}" defer></script>
+    <script src="{{ asset('js/jquery/jquery-form.min.js') }}" defer></script>
+    <script src="{{ asset('js/jquery/script.js') }}" defer></script>
 
     @yield('scripts')
 </body>
