baa259a129353f60d43e3dd1fb55923e8b14cade
Rio Wirawan <oriorasajambu@gmail.com>
2023-03-19 15:42:58 +0700

    fix: style pagination

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -4,7 +4,8 @@
             <span class="relative z-0 inline-flex gap-3">
                 {{-- Previous Page Link --}}
                 @if ($paginator->onFirstPage())
-                    <span role="button" class="relative inline-flex items-center py-2" aria-disabled="true" aria-label="{{ __('pagination.previous') }}">
+                    <span role="button" class="relative inline-flex items-center py-2" aria-disabled="true"
+                        aria-label="{{ __('pagination.previous') }}">
                         <span class="relative inline-flex items-center px-2 py-2" aria-hidden="true">
                             <svg xmlns="http://www.w3.org/2000/svg" width="25.144" height="14.382"
                                 viewBox="0 0 25.144 14.382">
@@ -27,11 +28,11 @@
                             viewBox="0 0 25.144 14.382">
                             <g id="Group_376" data-name="Group 376" transform="translate(-578.793 -2186.809)">
                                 <path id="Path_267" data-name="Path 267" d="M0,6.837,6.837,0l6.837,6.837"
-                                    transform="translate(579.5 2200.837) rotate(-90)" fill="none"
-                                    stroke="#404040" stroke-width="1" />
+                                    transform="translate(579.5 2200.837) rotate(-90)" fill="none" stroke="#404040"
+                                    stroke-width="1" />
                                 <path id="Path_268" data-name="Path 268" d="M1208.8,3931.5h-24.437"
-                                    transform="translate(1788.301 6125.5) rotate(180)" fill="none"
-                                    stroke="#404040" stroke-width="1" />
+                                    transform="translate(1788.301 6125.5) rotate(180)" fill="none" stroke="#404040"
+                                    stroke-width="1" />
                             </g>
                         </svg>
                     </a>
@@ -50,9 +51,10 @@
                     @if (is_array($element))
                         @foreach ($element as $page => $url)
                             @if ($page == $paginator->currentPage())
-                                <span role="button" class="h-full" aria-current="page">
-                                    <span
-                                        class="relative inline-flex items-center px-4 py-2 -ml-px font-dm-sans font-bold border-2 border-partial rounded-sm bg-primary text-white">{{ $page }}</span>
+                                <span role="button"
+                                    class="relative inline-flex items-center px-4 py-2 -ml-px font-dm-sans font-bold border-2 border-partial rounded-sm bg-primary text-white"
+                                    aria-current="page">
+                                    {{ $page }}
                                 </span>
                             @else
                                 <a href="{{ $url }}"
@@ -83,7 +85,8 @@
                         </svg>
                     </a>
                 @else
-                    <span role="button" class="relative inline-flex items-center py-2" aria-disabled="true" aria-label="{{ __('pagination.next') }}">
+                    <span role="button" class="relative inline-flex items-center py-2" aria-disabled="true"
+                        aria-label="{{ __('pagination.next') }}">
                         <span class="relative inline-flex items-center px-2 py-2 -ml-px" aria-hidden="true">
                             <svg xmlns="http://www.w3.org/2000/svg" width="25.144" height="14.382"
                                 viewBox="0 0 25.144 14.382">
