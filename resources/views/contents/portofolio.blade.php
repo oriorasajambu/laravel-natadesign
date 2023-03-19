c02e596033fd83608d107a16479c9086c6f05867
Rio Wirawan <oriorasajambu@gmail.com>
2023-03-19 17:43:42 +0700

    fix: modal

--- resources/views/contents/portofolio.blade.php
+++ resources/views/contents/portofolio.blade.php
@@ -106,9 +106,6 @@
             for (i = 0; i < slides.length; i++) {
                 slides[i].style.display = "none";
             }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
             slides[slideIndex - 1].style.display = "block";
         }
     </script>
