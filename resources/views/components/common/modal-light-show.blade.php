e1916e3419046548f120065318e9be22081bf758
Rio Wirawan <oriorasajambu@gmail.com>
2023-03-19 16:45:28 +0700

    fix: center arrow

--- resources/views/components/common/modal-light-show.blade.php
+++ resources/views/components/common/modal-light-show.blade.php
@@ -10,23 +10,9 @@
         @endforeach
 
         <!-- Next/previous controls -->
-        <a class="hover:cursor-pointer text-white text-xl absolute top-1/2 p-4 select-none"
+        <a class="hover:cursor-pointer text-white text-xl absolute top-1/2 translate-y-1/2 p-4 select-none"
             onclick="plusSlides(-1)">&#10094;</a>
-        <a class="hover:cursor-pointer text-white text-xl absolute top-1/2 right-0 p-4 select-none"
+        <a class="hover:cursor-pointer text-white text-xl absolute top-1/2 translate-y-1/2 right-0 p-4 select-none"
             onclick="plusSlides(1)">&#10095;</a>
-
-        <!-- Caption text -->
-        <div class="text-center bg-black text-white px-1 py-4">
-            <p id="caption"></p>
-        </div>
-
-        <!-- Thumbnail image controls -->
-        @foreach ($album->photos as $key => $photo)
-            <div class="float-left w-1/4">
-                <img class="demo opacity-60 hover:opacity-100" src="https://admin.natadesign.id/{{ $photo->original }}"
-                    loading="lazy" width="550" height="550" alt="{{ $photo->alt }}" style="width:100%"
-                    class="select-none" onclick="currentSlide({{ $key + 1 }})">
-            </div>
-        @endforeach
     </div>
 </div>
