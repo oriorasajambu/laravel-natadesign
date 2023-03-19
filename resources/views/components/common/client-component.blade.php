769476f37f62951aee2885be7917b8fb1622cb43
Rio Wirawan <oriorasajambu@gmail.com>
2023-03-19 14:17:18 +0700

    fix: client component

--- resources/views/components/common/client-component.blade.php
+++ resources/views/components/common/client-component.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-row flex-1 flex-nowrap overflow-x-auto lg:grid lg:grid-cols-5 md:grid md:grid-cols-4 sm:grid sm:grid-cols-3 lg:gap-x-6 md:gap-x-6 sm:gap-x-3 ssm:gap-x-3 xs:gap-x-2 w-full items-center">
+<div class="flex flex-row flex-1 flex-nowrap overflow-x-auto lg:grid lg:grid-cols-5 md:grid md:grid-cols-4 sm:grid sm:grid-cols-3 lg:gap-6 md:gap-6 sm:gap-3 ssm:gap-3 xs:gap-2 w-full items-center">
     @foreach ($clients as $client)
         <div class="grow w-full ssm:h-[250px] xs:h-[250px] border-2 border-primary aspect-square">
             <img class="w-full p-10" width="103" height="103" loading="lazy" src="https://admin.natadesign.id/{{ $client->image }}"
