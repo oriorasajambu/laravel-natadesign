0e2cd8fc6ee34b8b325cc480e8ff63d693647cdb
Rio Wirawan <oriorasajambu@gmail.com>
2023-03-19 15:26:39 +0700

    fix: accordion text

--- resources/views/components/common/accordion-component.blade.php
+++ resources/views/components/common/accordion-component.blade.php
@@ -2,11 +2,11 @@
     <input type="checkbox" aria-label="Toggle Collapse" class="peer absolute w-full h-full p-0 m-0 opacity-0 z-10 cursor-pointer" />
     <div class="flex flex-row justify-between w-full {{ $class }} ">
         @if ($position != -1)
-            <p class="basis-1/6 text-2xl ssm:xs:text-xl font-dm-sans text-partial">
+            <p class="basis-1/6 text-2xl ssm:text-xl xs:text-xl font-dm-sans text-partial">
                 {{ str_pad($position, 2, '0', STR_PAD_LEFT) }}
             </p>
         @endif
-        <p class="basis-5/6 text-2xl xs:text-xl font-dm-sans text-partial ">{{ $title }}</p>
+        <p class="basis-5/6 text-2xl ssm:text-xl xs:text-xl font-dm-sans text-partial ">{{ $title }}</p>
     </div>
     <div class="self-end absolute transition-transform duration-500 rotate-0 peer-checked:rotate-90 lg:mr-12 md:mr-12 sm:mr-12 xs:mr-0">
         <svg xmlns="http://www.w3.org/2000/svg" width="14.12" height="25.414" viewBox="0 0 14.12 25.414">
