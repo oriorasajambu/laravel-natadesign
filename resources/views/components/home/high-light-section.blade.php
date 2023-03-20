b2cd466eef5195d473915d989657a4bf05d58dff
Rio Wirawan <oriorasajambu@gmail.com>
2023-03-20 13:56:20 +0700

    fix: hightlight section

--- resources/views/components/home/high-light-section.blade.php
+++ resources/views/components/home/high-light-section.blade.php
@@ -31,7 +31,7 @@
                         </div>
                     </div>
                     <a aria-label="See Our Gallery" href="/portofolio"
-                        class="flex sm:flex-1 ssm:flex-1 ssm:justify-between xs:flex-1 xs:justify-between items-center text-[#404040] font-dm-sans font-bold">
+                        class="flex ssm:flex-1 ssm:justify-between xs:flex-1 xs:justify-between items-center text-[#404040] font-dm-sans font-bold">
                         <p>Lihat Gallery Kami</p>
                         <div>
                             <svg xmlns="http://www.w3.org/2000/svg" class="text-primary ml-6" width="25.144"
