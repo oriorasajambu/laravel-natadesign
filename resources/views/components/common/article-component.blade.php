b53e168e8451b7e2dfb4d7adc1721a0b0db2bb61
Rio Wirawan <oriorasajambu@gmail.com>
2023-03-21 11:14:56 +0700

    fix: text size

--- resources/views/components/common/article-component.blade.php
+++ resources/views/components/common/article-component.blade.php
@@ -9,7 +9,7 @@
                         <img src="{{ isset($article->thumbnail->image) ? 'https://admin.natadesign.id/' . $article->thumbnail->image : asset('assets/favicon.svg') }}"
                             class="basis-[19%] w-full grow aspect-square" loading="lazy"
                             alt="{{ isset($article->thumbnail->alt) ? $article->thumbnail->alt : $article->title }}" />
-                        <h2 class="text-partial font-dm-sans font-bold text-xl truncate">{{ $article->title }}</h2>
+                        <h2 class="text-partial font-dm-sans font-bold lg:text-xl md:text-xl sm:text-xl ssm:text-base xs:text-base truncate">{{ $article->title }}</h2>
                         <div class="text-partial font-dm-sans text-base leading-6 line-clamp-3">
                             {!! $article->content !!}
                         </div>
@@ -20,7 +20,7 @@
                     href="{{ $article->category->name == 'Ulasan Media' ? '/media' : '/articles' }}/{{ $article->slug }}">
                     <article class="flex flex-col gap-6">
                         <x-common.date-component date='{{ $article->published_at }}' />
-                        <h2 class="text-partial font-dm-sans font-bold text-xl truncate">{{ $article->title }}</h2>
+                        <h2 class="text-partial font-dm-sans font-bold lg:text-xl md:text-xl sm:text-xl ssm:text-base xs:text-base truncate">{{ $article->title }}</h2>
                         <div class="text-partial font-dm-sans text-base leading-6 line-clamp-3">
                             {!! $article->content !!}
                         </div>
@@ -33,7 +33,7 @@
             <a aria-label={{ $article->title }} class="lg:hidden md:hidden sm:hidden ssm:block xs:block"
                 href="{{ $article->category->name == 'Ulasan Media' ? '/media' : '/articles' }}/{{ $article->slug }}">
                 <article class="flex flex-col lg:gap-6 md:gap-6 ssm:gap-3 sm:gap-3 xs:gap-3">
-                    <h2 class="text-partial font-dm-sans font-bold text-xl truncate">{{ $article->title }}</h2>
+                    <h2 class="text-partial font-dm-sans font-bold lg:text-xl md:text-xl sm:text-xl ssm:text-base xs:text-base truncate">{{ $article->title }}</h2>
                     <x-common.date-component date='{{ $article->published_at }}' />
                     <div class="text-partial font-dm-sans text-base leading-6 line-clamp-3">
                         {!! $article->content !!}
