03c531604af44546237dcf5b0de42b800672722a
Rio Wirawan <oriorasajambu@gmail.com>
2023-04-13 11:32:44 +0700

    fix: crash

--- resources/views/components/common/article-component.blade.php
+++ resources/views/components/common/article-component.blade.php
@@ -34,19 +34,19 @@
                     </article>
                 </a>
             @endif
+            <a aria-label={{ $article->title }} class="lg:invisible md:invisible sm:invisible ssm:block xs:block"
+                href="{{ $article->category->name == 'Ulasan Media' ? '/media' : '/articles' }}/{{ $article->slug }}">
+                <article class="flex flex-col lg:gap-6 md:gap-6 ssm:gap-3 sm:gap-3 xs:gap-3">
+                    <h2 class="text-partial font-dm-sans font-bold lg:text-xl md:text-xl sm:text-xl ssm:text-base xs:text-base truncate">{{ $article->title }}</h2>
+                    <x-common.date-component date='{{ $article->published_at }}' />
+                    <div class="text-partial font-dm-sans text-base leading-6 line-clamp-3">
+                        {!! $article->content !!}
+                    </div>
+                    <img src="{{ isset($article->thumbnail->image) ? 'https://admin.natadesign.id/' . $article->thumbnail->image : asset('assets/favicon.svg') }}"
+                        class="basis-[19%] w-full grow aspect-square" loading="lazy"
+                        alt="{{ isset($article->thumbnail->alt) ? $article->thumbnail->alt : $article->title }}" />
+                </article>
+            </a>
         @endforeach
-        <a aria-label={{ $article->title }} class="lg:invisible md:invisible sm:invisible ssm:block xs:block"
-            href="{{ $article->category->name == 'Ulasan Media' ? '/media' : '/articles' }}/{{ $article->slug }}">
-            <article class="flex flex-col lg:gap-6 md:gap-6 ssm:gap-3 sm:gap-3 xs:gap-3">
-                <h2 class="text-partial font-dm-sans font-bold lg:text-xl md:text-xl sm:text-xl ssm:text-base xs:text-base truncate">{{ $article->title }}</h2>
-                <x-common.date-component date='{{ $article->published_at }}' />
-                <div class="text-partial font-dm-sans text-base leading-6 line-clamp-3">
-                    {!! $article->content !!}
-                </div>
-                <img src="{{ isset($article->thumbnail->image) ? 'https://admin.natadesign.id/' . $article->thumbnail->image : asset('assets/favicon.svg') }}"
-                    class="basis-[19%] w-full grow aspect-square" loading="lazy"
-                    alt="{{ isset($article->thumbnail->alt) ? $article->thumbnail->alt : $article->title }}" />
-            </article>
-        </a>
     </div>
 </div>
