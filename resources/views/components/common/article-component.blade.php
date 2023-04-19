427dde8d8dd9d97f4793be0d080f9e6968ffffb8
Rio Wirawan <oriorasajambu@gmail.com>
2023-04-19 10:11:02 +0700

    fix: layouting article section

--- resources/views/components/common/article-component.blade.php
+++ resources/views/components/common/article-component.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-col gap-6">
-    <div class="grid grid-cols-3 xs:grid-cols-1 lg:gap-x-6 md:gap-x-6 sm:gap-x-6 ssm:gap-x-8 xs:gap-x-8 lg:gap-y-12 md:gap-y-12 sm:gap-y-12 ssm:gap-y-12 xs:gap-y-12 lg:mt-20 mt-10">
+    <div class="grid grid-cols-3 xs:grid-cols-1 lg:gap-x-6 md:gap-x-6 sm:gap-x-6 ssm:gap-x-8 xs:gap-x-8 lg:gap-y-12 md:gap-y-14 sm:gap-y-14 ssm:gap-y-14 xs:gap-y-14 lg:mt-20 mt-10">
         @foreach ($articles as $key => $article)
             @if ($key % 2 == 0)
                 <article class="relative lg:block md:block sm:block ssm:hidden xs:hidden">
@@ -12,7 +12,7 @@
                             class="basis-[19%] w-full grow aspect-square mb-6" loading="lazy"
                             alt="{{ isset($article->thumbnail->alt) ? $article->thumbnail->alt : $article->title }}" />
                         <h2
-                            class="text-partial font-dm-sans font-bold lg:text-xl md:text-xl sm:text-xl ssm:text-base xs:text-base truncate mb-3">
+                            class="text-partial font-dm-sans font-bold lg:text-xl md:text-xl sm:text-xl ssm:text-base xs:text-base truncate mb-1">
                             {{ $article->title }}</h2>
                         <div class="text-partial font-dm-sans text-base leading-6 line-clamp-3">
                             {!! $article->content !!}
@@ -27,7 +27,7 @@
                     <div class="flex flex-col">
                         <x-common.date-component date='{{ $article->published_at }}' class="mb-3" />
                         <h2
-                            class="text-partial font-dm-sans font-bold lg:text-xl md:text-xl sm:text-xl ssm:text-base xs:text-base truncate mb-3">
+                            class="text-partial font-dm-sans font-bold lg:text-xl md:text-xl sm:text-xl ssm:text-base xs:text-base truncate mb-1">
                             {{ $article->title }}</h2>
                         <div class="text-partial font-dm-sans text-base leading-6 line-clamp-3 mb-3">
                             {!! $article->content !!}
@@ -45,7 +45,7 @@
                     class="absolute w-full h-full"></a>
                 <div class="flex flex-col">
                     <h2
-                        class="text-partial font-dm-sans font-bold lg:text-xl md:text-xl sm:text-xl ssm:text-base xs:text-base truncate mb-3">
+                        class="text-partial font-dm-sans font-bold lg:text-xl md:text-xl sm:text-xl ssm:text-base xs:text-base truncate mb-1">
                         {{ $article->title }}</h2>
                     <x-common.date-component date='{{ $article->published_at }}' class="mb-3" />
                     <div class="text-partial font-dm-sans text-base leading-6 line-clamp-3 mb-6">
